23dbf9100acc19353202dae151b51a11ead76208
neodevelop <neodevelop@gmail.com>
2011-03-12 17:05:36 -0600

    Simple impl for search tag

--- grails-app/taglib/mx/org/grails/FlickrTagLib.groovy
+++ grails-app/taglib/mx/org/grails/FlickrTagLib.groovy
@@ -1,5 +1,31 @@
 package mx.org.grails
 
 class FlickrTagLib {
-
+  
+  def flickrService
+  
+  def pluginManager
+  
+  static namespace = "flickr"
+  
+  // This taglib receives tag, perPage, cols
+  def search = { attrs, body ->
+    
+    def plugin = pluginManager.getGrailsPlugin('flickr')
+    
+    def urls = flickrService.search(attrs.tag,attrs.perPage)
+    out << "<table id='${attrs.tag}'><tr>"
+    def contador = 0
+    urls.each{ url ->
+      def urlModificada = url.replace("_s.jpg","_b.jpg")
+      out << "<td> <a href='${urlModificada}'><img src='${url}' /></a> </td>"
+      contador++
+      if((contador % (attrs?.cols as Integer ?: 3)) == 0){
+        out << "</tr><tr>"
+      }
+    }
+    
+    out << "</tr></table>"
+  }
+  
 }
---
+++ grails-app/views/flickr/_searchResult.gsp
@@ -0,0 +1,11 @@
+<table>
+  <tr>
+  <!--g:each in="${urls}" var="url" status="i"-->
+    <td><img src="" alt="img" title="img"/></td>
+      <!--g:if test="${i%cols==0}"-->  
+        </tr>
+        <tr>
+      <!--/g:if-->
+  <!--/g:each-->
+  </tr>
+</table>
