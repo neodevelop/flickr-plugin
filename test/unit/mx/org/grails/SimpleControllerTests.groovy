7fe02d5047fb6a269d97ee17f56d22fb518f0633
neodevelop <neodevelop@gmail.com>
2011-03-23 02:52:57 -0600

    sample controller for check it works

---
+++ test/unit/mx/org/grails/SimpleControllerTests.groovy
@@ -0,0 +1,17 @@
+package mx.org.grails
+
+import grails.test.*
+
+class SimpleControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
