375f195d9485023a221499d2b3b8afe7660e3ea7
neodevelop <neodevelop@gmail.com>
2011-03-26 22:27:39 -0600

    We add a test page

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,7 @@
 class UrlMappings {
 
 	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
+		"/flickrSample"(controller:"flickr")
 
 		"/"(view:"/index")
 		"500"(view:'/error')
