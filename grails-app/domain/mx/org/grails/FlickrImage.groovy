dad467a726e9171c174072bc6c441753e9448d08
Edd Grant <edd@eddgrant.com>
2011-11-29 17:55:13 +0000

    Further changes

--- grails-app/domain/mx/org/grails/FlickrImage.groovy
+++ grails-app/domain/mx/org/grails/FlickrImage.groovy
@@ -4,8 +4,19 @@
  * @author egrant
  *
  */
-package com.eddgrant
+package mx.org.grails
 
+/**
+ * TODO: Add a method which returns the page URL of the media, see:
+ * 1: http://www.bram.us/2008/01/12/my-priceless-flickr-tip-how-to-find-the-original-flickr-photo-url-and-user-from-a-static-flickr-image-url/
+ * and
+ * 2: http://www.flickr.com/photos/83399230@N00/2177060015/ (just an example page url)
+ * 
+ * TODO: Add another method which takes is to the 'all sizes' page for the media.
+ * 
+ * @author egrant
+ *
+ */
 class FlickrImage {
 
   static constraints = {
@@ -59,27 +70,27 @@ class FlickrImage {
 
   boolean publicMedia
  
-  public String getUrl() {
+  def getUrl() {
     String url = new String(IMAGE_URL_STANDARD)
-    url = url.replace(FARM_ID_TOKEN, farmId())
-    url = url.replace(SERVER_ID_TOKEN, serverId())
-    url = url.replace(ID_TOKEN, mediaId())
-    url = url.replace(SECRET_TOKEN, secret())
+    url = url.replace(FARM_ID_TOKEN, farmId)
+    url = url.replace(SERVER_ID_TOKEN, serverId)
+    url = url.replace(ID_TOKEN, mediaId)
+    url = url.replace(SECRET_TOKEN, secret)
     return url
   }
 
-  public String getUrl(SizeSuffix sizeSuffix) {
+  def getUrl(SizeSuffix sizeSuffix) {
     String url = new String(IMAGE_URL_SUFFIXED)
-    url = url.replace(FARM_ID_TOKEN, farmId())
-    url = url.replace(SERVER_ID_TOKEN, serverId())
-    url = url.replace(ID_TOKEN, mediaId())
-    url = url.replace(SECRET_TOKEN, secret())
+    url = url.replace(FARM_ID_TOKEN, farmId)
+    url = url.replace(SERVER_ID_TOKEN, serverId)
+    url = url.replace(ID_TOKEN, mediaId)
+    url = url.replace(SECRET_TOKEN, secret)
     url = url.replace(SIZE_SUFFIX, sizeSuffix.getSuffix())
     return url
   }
 }
 
-enum SizeSuffix {
+public enum SizeSuffix {
   SMALL_SQUARE("s"),
   THUMBNAIL("t"),
   SMALL("m"),
@@ -95,5 +106,3 @@ enum SizeSuffix {
     return suffix
   }
 }
-
-
