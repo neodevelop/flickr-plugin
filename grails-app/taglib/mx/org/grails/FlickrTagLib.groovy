48979af3e8522ed0151d911e7adc61f2509cb0c9
neodevelop <neodevelop@gmail.com>
2011-12-02 18:26:20 -0600

    Adjusting taglib
    -Using the data from service
    -Using 'tags' instead 'tag'

--- grails-app/taglib/mx/org/grails/FlickrTagLib.groovy
+++ grails-app/taglib/mx/org/grails/FlickrTagLib.groovy
@@ -1,3 +1,18 @@
+/* Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package mx.org.grails
 
 class FlickrTagLib {
@@ -14,13 +29,13 @@ class FlickrTagLib {
         template:'/flickr/searchResult',
         plugin:'flickr',
         model:[
-          urls:data.urls,
-          tag:attrs.tag,
-          page:data.page[0],
-          pages:data.pages[0],
+          photos:data.photos,
+          tag:attrs.tags.replace(',','_'),
+          page:data.page,
+          pages:data.pages,
           perPage:attrs.perPage as Integer ?: 12,
           animated:attrs.animated ?: false,
-          id:"${attrs.tag}${new Date().time}"
+          id:"${attrs.tags.replace(',','_')}${new Date().time}"
         ])
     }catch(FlickrException e){
       out << "<h6> ${e.message} </h6>"
