c9b56799d1608221c288bcd16b3670ed4624d7b7
neodevelop <neodevelop@gmail.com>
2011-12-02 18:30:55 -0600

    Adjusting service and making test
    -we use only a list of FlickrImage types
    -move the config to constructor
    -In test, mocking the result of FlickrSearch and parser

--- grails-app/services/mx/org/grails/FlickrService.groovy
+++ grails-app/services/mx/org/grails/FlickrService.groovy
@@ -1,3 +1,18 @@
+/* Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package mx.org.grails
 
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
@@ -8,13 +23,13 @@ class FlickrService {
   static transactional = false
   
   XmlParser xmlParser
+  String api_key
   
   public FlickrService() {
     xmlParser = new XmlParser()
+    //  API key obtained from Config
+    api_key = ConfigurationHolder.config.grails.plugins.flickr.apiKey
   }
-
-  //  API key obtained from Config
-  def api_key = ConfigurationHolder.config.grails.plugins.flickr.apiKey
 
   def search(closure) {
     // Check if exists an api key
@@ -33,7 +48,7 @@ class FlickrService {
   }
   
   private def fetchXmlFromFlickr(api_url){
-    List<FlickrImage> media = new ArrayList<FlickrImage>();
+    List<FlickrImage> flickrImages = new ArrayList<FlickrImage>();
     
     // We use the parser to obtain XML
     def rsp = xmlParser.parse(api_url)
@@ -42,7 +57,7 @@ class FlickrService {
       throw new FlickrException(message:rsp.err.'@msg'[0])
     }
     else{
-      // Si no arrojo error empezamos a iterar nuestro objeto XML con uso de XmlParser
+      // If there's no error so we start to iterate
       rsp.photos.photo.each{
         def mediaId = it.@id
         def farmId = it.@farm
@@ -51,18 +66,16 @@ class FlickrService {
         def title = it.@title
         def publicMedia = it.@ispublic == 1 ? true : false
         
-        // Generamos ligas e imagenes basados en los resultados del consumo REST
+        // We generate links and images based on results of REST consume
         def flickrImage = new FlickrImage(mediaId: mediaId, farmId: farmId, serverId: serverId,
                                           secret: secret, title: title, publicMedia: publicMedia)
         
-        //TODO: The collection of URL Strings is now superceded by the FlickrImage List. Should we remove this?
-        urls << flickrImage.getUrl(SizeSuffix.SMALL)
-        media.add(flickrImage)   					
+        flickrImages.add(flickrImage)   					
       }	
     }
     
     long currentPage = rsp.photos.'@page'[0] as long
     long totalPages =  rsp.photos.'@pages'[0] as long
-    [urls: urls, media: media, page: currentPage, pages: totalPages]
+    [photos: flickrImages, page: currentPage, pages: totalPages]
   }
 }
--- test/unit/mx/org/grails/FlickrServiceTests.groovy
+++ test/unit/mx/org/grails/FlickrServiceTests.groovy
@@ -1,44 +1,71 @@
 /* Copyright 2011 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package mx.org.grails
 
 import grails.test.*
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 class FlickrServiceTests extends GrailsUnitTestCase {
-  
-    def flickrService
-  
-    protected void setUp() {
-        super.setUp()
-        flickrService = new FlickrService()
-        
-        /*def mockXmlParser = new MockFor(XmlParser)
-        mockXmlParser.demand.parse {
-          return "this is my xml"
+
+  def flickrService
+
+  protected void setUp() {
+    super.setUp()
+    mockConfig('''
+      grails{
+        plugins{
+          flickr{
+            apiKey = 'YOUR_API_KEY'
+          }
         }
-        
-        flickrService.setXmlParser(mockXmlParser.proxyInstance())*/
+      }
+    ''')
+  }
+
+  protected void tearDown() {
+    super.tearDown()
+  }
+
+  void testSearch() {
+
+    def flickrSearchMock = mockFor(FlickrSearch)
+    flickrSearchMock.demand.static.buildUrlToSearch{api_key,closure ->
+      "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=YOUR_API_KEY"
+    }
+    
+    def xml = '''
+    <rsp stat="ok">
+    <photos page="1" pages="2" perpage="100" total="114">
+    	<photo id="3982370167" owner="29282267@N02" secret="6295d375ff" server="2487" farm="3" title="my_title" ispublic="1" isfriend="0" isfamily="0" />
+    </photos>
+    </rsp>
+    '''
+    def document = new XmlParser().parseText(xml)
+    
+    def mockXmlParser = mockFor(XmlParser)
+    mockXmlParser.demand.parse { api_url ->
+      document
     }
 
-    protected void tearDown() {
-        super.tearDown()
-    }
+    flickrService = new FlickrService()
+    flickrService.xmlParser = mockXmlParser.createMock() 
 
-    void testSearch() {
-      def urls = flickrService.search("springhispano")
-      assert urls.size() > 0
+    def data = flickrService.search{
+      tags "springhispano"
     }
+    assert data.photos.size() == 1
+  }
 }
